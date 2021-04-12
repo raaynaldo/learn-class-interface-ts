a2f65d0e9c92267712be0218f97401b7d42f5ccf
raaynaldo <raaynaldo@hotmail.com>
2021-04-11 22:44:08 -0400

    first class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department('Ray');
+
+console.log(accounting);
