140679498c914c37c6617b1e127feb9c996ba235
raaynaldo <raaynaldo@hotmail.com>
2021-04-16 22:57:50 -0400

    getter

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value');
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
+    this.lastReport = reports[0];
   }
 
   addEmployee(name: string) {
@@ -47,6 +64,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -63,6 +81,9 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
+
+accounting.mostRecentReport = 'hello';
+console.log(accounting.mostRecentReport);
 accounting.addReport('Someting went wrong...');
 
 accounting.addEmployee('Max');
