4dc7a18060df57604230c8d9f28832c6d8b8794a
raaynaldo <raaynaldo@hotmail.com>
2021-04-17 23:02:36 -0400

    static

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 class Department {
+  static fiscalYear = 2020;
   //   private readonly id: string;
   //   private name: string;
   protected employees: string[] = [];
@@ -6,6 +7,10 @@ class Department {
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -71,6 +76,9 @@ class AccountingDepartment extends Department {
     console.log(this.reports);
   }
 }
+
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
 
 const it = new ITDepartment('d1', ['Ray']);
 it.describe();
