c092f36137481d24c5cf7d337b91a630b0acce6f
raaynaldo <raaynaldo@hotmail.com>
2021-04-18 20:56:49 -0400

    implement to a class.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: 'Max',
-  age: 24,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+user1 = new Person('Max');
+user1.greet('Hello!');
+// user1 = {
+//   name: 'Max',
+//   age: 24,
+//   greet(phrase: string) {
+//     console.log(phrase + ' ' + this.name);
+//   },
+// };
